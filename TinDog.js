85949bbc43ea7b105b04e0a2c64aafe90b0b649d
snehiljain34 <54562587+snehiljain34@users.noreply.github.com>
2020-06-28 22:00:39 +0530

    Add files via upload

---
+++ TinDog.js
@@ -0,0 +1,3 @@
+alert("Hey there! I'm Snehil, this is the website created just like Tinder, for your beloved dogs.");
+var user_name = prompt("Hey, What's your name?");
+alert("hey "+user_name+" Now it's easy to arrange meet-ups for your Dogs!");
