966e33f3363aea5c8b54e0dbf6de6143026b7e61
snehiljain34 <54562587+snehiljain34@users.noreply.github.com>
2020-10-15 15:17:07 +0530

    Revert "added link  to about me"

--- TinDog.js
+++ TinDog.js
@@ -1,4 +1,3 @@
 alert("Hey there! I'm Snehil, this is the website created just like Tinder, for your beloved dogs.");
 var user_name = prompt("Hey, What's your name?");
-var user_lastname = prompt("hey , what's your last name?");
-alert("hey "+user_name+user_lastname" Now it's easy to arrange meet-ups for your Dogs!");
+alert("hey "+user_name+" Now it's easy to arrange meet-ups for your Dogs!");
